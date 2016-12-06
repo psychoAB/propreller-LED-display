1d657556507ca3d99bdc9b99cd97ce467f0d0e88
psychoAB <dome.2499@hotmail.com>
2016-12-06 22:54:45 +0700

    regain knowlegde by writing blinking LED

--- main.c
+++ main.c
@@ -1,4 +1,25 @@
+#include <avr/io.h>
+#include <util/delay.h>
+
 int main(void)
 {
+    DDRD |= (1 << PD3);
+    
+    uint8_t ledState = 0;
+
+    while(1)
+    {
+        if(ledState)
+        {
+            PORTD |= (1 << PD3);
+        }
+        else
+        {
+            PORTD &= !(1 << PD3);
+        }
+        ledState = !ledState;
+        _delay_ms(500);
+    }
+
     return 0;
 }
